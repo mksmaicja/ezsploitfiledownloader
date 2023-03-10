2ac7f26ab1d4d4601fb38444ff0d9d182ad955d2
mikusgszyp <118010017+mikusgszyp@users.noreply.github.com>
2023-03-10 11:12:39 +0100

    Update internalexecute.lua

--- internalexecute.lua
+++ internalexecute.lua
@@ -7,18 +7,17 @@ Duration = 5;
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("MIKQ HUB", "Synapse")
+local Window = Library.CreateLib("EzSplot Internal", "Synapse")
 
 --Main
 local Tab = Window:NewTab("Main")
 local MainSection = Tab:NewSection("cheats")
 
-Section:scripttextbox("Type script here", "TextboxInfo", function(txt)
-	print(txt)
+MainSection:scripttextbox("Type script here", "TextboxInfo", function(txt)
+	
 end)
 
 
-SectionMain:NewButton("ButtonText", "ButtonInfo", function()
-    loadstring(scripttextbox)()
-    end)
+MainSection:NewButton("ButtonText", "ButtonInfo", function()
+    loadstring(scripttextbox.Text)()
 end)

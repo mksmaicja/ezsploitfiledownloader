42e7b95ed724d92a6b9b8bfd257790d818a47e34
mikusgszyp <118010017+mikusgszyp@users.noreply.github.com>
2023-03-10 11:29:05 +0100

    Update internalexecute.lua

--- internalexecute.lua
+++ internalexecute.lua
@@ -17,7 +17,35 @@ MainSection:NewTextBox("Type script here", "TextboxInfo", function(txt)
 	
 end)
 
-
 MainSection:NewButton("Execute", "ButtonInfo", function()
     loadstring(scripttextbox.Text)()
+end)
+
+MainSection:NewButton("Btools", "add Btools to your inventory", function()
+    for i=2,4 do
+    local btool = Instance.new("HopperBin")
+    btool.BinType=i
+    btool.Parent=game:service'Players'.LocalPlayer.Backpack
+end
+
+mouse = game:service'Players'.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Unlock tool"
+tool.Activated:connect(function()
+    if mouse.Target:IsA("BasePart") then
+        mouse.Target.Locked=false
+        print("Unlocked "..mouse.Target:GetFullName())
+    end
+end)
+tool.Parent = game:service'Players'.LocalPlayer.Backpack
+end)
+
+MainSection:NewButton("inf jump", "infinite jump", function()
+    local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)
 end)

9ef6c263713eb750f6cc567d0d01f30954c3bedc
mikusgszyp <mikqru@gmail.com>
2023-06-10 20:44:24 +0200

    kys2

--- gejinject.lua
+++ gejinject.lua
@@ -1,75 +1,32 @@
 local ScreenGui = Instance.new("ScreenGui")
-local TextLabel = Instance.new("TextLabel")
+local Frame = Instance.new("Frame")
+local _1 = Instance.new("TextLabel")
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-TextLabel.Parent = ScreenGui
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0, 0, 0.93726474, 0)
-TextLabel.Size = UDim2.new(0, 119, 0, 50)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "EzSploit"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextSize = 40.000
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame.BackgroundTransparency = 0.250
+Frame.Position = UDim2.new(0.863671064, 0, 0.830614805, 0)
+Frame.Size = UDim2.new(0, 261, 0, 74)
+_1.Name = "1"
+_1.Parent = Frame
+_1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_1.BackgroundTransparency = 1.000
+_1.Position = UDim2.new(0.00766283507, 0, -0.0135135138, 0)
+_1.Size = UDim2.new(0, 259, 0, 75)
+_1.Font = Enum.Font.SourceSans
+_1.Text = "EzSploit Injected OMG!!?!?!?"
+_1.TextColor3 = Color3.fromRGB(250, 250, 250)
+_1.TextSize = 23.000
 
-local function Sex() -- TextLabel.LocalScript 
-	local script = Instance.new('LocalScript', TextLabel)
+wait(2.5)
 
-	local text = script.Parent
-	local add = 15
-	wait()
-	local k = 1
-	while k <= 255 do
-		text.TextColor3 = Color3.new(k/255,0/255,0/255)
-		k = k + add
-		wait()
-	end
-	while true do
-		k = 1
-		while k <= 255 do
-			text.TextColor3 = Color3.new(255/255,k/255,0/255)
-			k = k + add
-			wait()
-		end
-		k = 1
-		while k <= 255 do
-			text.TextColor3 = Color3.new(255/255 - k/255,255/255,0/255)
-			k = k + add
-			wait()
-		end
-		k = 1
-		while k <= 255 do
-			text.TextColor3 = Color3.new(0/255,255/255,k/255)
-			k = k + add
-			wait()
-		end
-		k = 1
-		while k <= 255 do
-			text.TextColor3 = Color3.new(0/255,255/255 - k/255,255/255)
-			k = k + add
-			wait()
-		end
-		k = 1
-		while k <= 255 do
-			text.TextColor3 = Color3.new(k/255,0/255,255/255)
-			k = k + add
-			wait()
-		end
-		k = 1
-		while k <= 255 do
-			text.TextColor3 = Color3.new(255/255,0/255,255/255 - k/255)
-			k = k + add
-			wait()
-		end
-		while k <= 255 do
-			text.TextColor3 = Color3.new(255/255 - k/255,0/255,0/255)
-			k = k + add
-			wait()
-		end
-	end
-end
-coroutine.wrap(Sex)()
+_1.Text = "EzSploit by mikusdev"
 
-wait(5)
-ScreenGui:Destroy()
+wait(2.5)
+
+_1.Text = "Script by nicknamez?!?!?!"
+
+wait(2.5)
+Frame:Destroy()
+_1:Destroy()
